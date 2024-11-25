c8f5066cbe1e156fb05d12f55ff1ea98c4c7b8ff
lede-dev <lede.7716@gmail.com>
2024-11-26 04:21:50 +0900

    feat: added chzzk crawler

---
+++ src/main/java/net/ledestudios/streambridge/chzzk/Chzzk.java
@@ -0,0 +1,47 @@
+package net.ledestudios.streambridge.chzzk;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class Chzzk {
+
+    private final @Nullable String aut;
+    private final @Nullable String ses;
+
+    public static @NotNull Chzzk anonymous() {
+        return new Chzzk(null, null);
+    }
+
+    public static @NotNull Chzzk login(@NotNull String aut, @NotNull String ses) {
+        return new Chzzk(aut, ses);
+    }
+
+    public static @NotNull Chzzk login(@NotNull Naver naver) {
+        if (naver.isAnonymous()) {
+            throw new IllegalArgumentException("Must be logged in to Naver.");
+        }
+        return new Chzzk(naver.getAutToken(), naver.getSesToken());
+    }
+
+    private Chzzk(@Nullable String aut, @Nullable String ses) {
+        this.aut = aut;
+        this.ses = ses;
+    }
+
+    public @NotNull ChzzkChannel getChannel(@NotNull String channelId) {
+        return new ChzzkChannel(this, channelId);
+    }
+
+    public boolean isAnonymous() {
+        return aut == null || ses == null;
+    }
+
+    public @Nullable String getAutToken() {
+        return aut;
+    }
+
+    public @Nullable String getSesToken() {
+        return ses;
+    }
+
+}
---
+++ src/main/java/net/ledestudios/streambridge/chzzk/ChzzkChannel.java
@@ -0,0 +1,32 @@
+package net.ledestudios.streambridge.chzzk;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import net.ledestudios.streambridge.net.ChzzkHttpService;
+import org.jetbrains.annotations.NotNull;
+
+public class ChzzkChannel {
+
+    private final Gson gson = new Gson();
+
+    private final @NotNull ChzzkHttpService http;
+    private final @NotNull String channel;
+
+    ChzzkChannel(@NotNull Chzzk chzzk, @NotNull String channel) {
+        this.http = new ChzzkHttpService(chzzk);
+        this.channel = channel;
+    }
+
+    public @NotNull ChzzkLiveDetail getLiveDetail() {
+        String url = String.format("https://api.chzzk.naver.com/service/v2/channels/%s/live-detail", channel);
+        JsonElement json = http.get(url);
+        return gson.fromJson(json, ChzzkLiveDetail.class);
+    }
+
+    public @NotNull ChzzkLiveStatus getLiveStatus() {
+        String url = String.format("https://api.chzzk.naver.com/polling/v2/channels/%s/live-status", channel);
+        JsonElement json = http.get(url);
+        return gson.fromJson(json, ChzzkLiveStatus.class);
+    }
+
+}
