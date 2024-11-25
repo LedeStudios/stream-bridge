9fe0e3f58eb0928eb0235bc5a540c532838e7655
lede-dev <lede.7716@gmail.com>
2024-11-26 02:52:02 +0900

    feat: add chrome driver utility

---
+++ src/main/java/net/ledestudios/streambridge/utils/Chrome.java
@@ -0,0 +1,30 @@
+package net.ledestudios.streambridge.utils;
+
+import org.jetbrains.annotations.NotNull;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class Chrome {
+
+    public static void setDriverProperty(@NotNull String path) {
+        System.setProperty("webdriver.chrome.driver", path);
+    }
+
+    public static @NotNull WebDriver getDriver() {
+        try {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            WebDriver driver = new ChromeDriver(options);
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            return driver;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
