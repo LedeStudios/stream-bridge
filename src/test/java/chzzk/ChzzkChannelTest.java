7bc542b4ce6b3890f2cf16983a0caa4b8ed0b5db
lede-dev <lede.7716@gmail.com>
2024-11-27 11:52:34 +0900

    test: added chzzk channel test

---
+++ src/test/java/chzzk/ChzzkChannelTest.java
@@ -0,0 +1,58 @@
+package chzzk;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import net.ledestudios.streambridge.chzzk.Chzzk;
+import net.ledestudios.streambridge.chzzk.ChzzkChannel;
+import net.ledestudios.streambridge.chzzk.Naver;
+import net.ledestudios.streambridge.chzzk.type.ChzzkFollower;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class ChzzkChannelTest {
+
+    private final @NotNull String CHANNEL_TO_TEST = "46a5788917ee53a4a46f634d536bd238";
+
+    private final @NotNull Dotenv env = Dotenv.configure().filename(".env").load();
+
+    @Test
+    public void testChzzkLiveStatus() {
+        Assertions.assertDoesNotThrow(() -> {
+            final ChzzkChannel channel = Chzzk.anonymous().getChannel(CHANNEL_TO_TEST);
+            System.out.println("LiveStatus: " + channel.getLiveStatus());
+        });
+    }
+
+    @Test
+    public void testChzzkLiveDetail() {
+        Assertions.assertDoesNotThrow(() -> {
+           final ChzzkChannel channel = Chzzk.anonymous().getChannel(CHANNEL_TO_TEST);
+           System.out.println("LiveDetail: " + channel.getLiveDetail());
+        });
+    }
+
+    @Test
+    public void testChzzkFollowerCount() {
+        Assertions.assertDoesNotThrow(() -> {
+            Naver.loginAsync(env.get("NAVER_ID"), env.get("NAVER_PW")).thenAccept(naver -> {
+                final ChzzkChannel channel = Chzzk.login(naver).getChannel(CHANNEL_TO_TEST);
+                System.out.println("FollowerCount: " + channel.getFollowerCount());
+            }).join();
+        });
+    }
+
+    @Test
+    public void testChzzkFollowers() {
+        Assertions.assertDoesNotThrow(() -> {
+            Naver.loginAsync(env.get("NAVER_ID"), env.get("NAVER_PW")).thenAccept(naver -> {
+                final ChzzkChannel channel = Chzzk.login(naver).getChannel(CHANNEL_TO_TEST);
+                List<ChzzkFollower> followers = channel.getFollowers();
+                System.out.println("FollowerCount: " + followers.size());
+                System.out.println("Followers: " + followers);
+            }).join();
+        });
+    }
+
+}
